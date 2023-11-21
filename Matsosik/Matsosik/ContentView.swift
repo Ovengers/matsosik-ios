609d3fd8248bd88c2fb6f2387fa7fc899fd4beae
kdaramz <be9.smile@gmail.com>
2023-11-22 02:21:19 +0900

    Initial commit

---
+++ Matsosik/Matsosik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Matsosik
+//
+//  Created by Kdaramz on 11/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Matsosik/Matsosik/MatsosikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatsosikApp.swift
+//  Matsosik
+//
+//  Created by Kdaramz on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MatsosikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
